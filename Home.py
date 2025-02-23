b2dcc12410fdc81ef02b0e806fe4f377138b58ef
parthiban <55436038+parthibanvthink@users.noreply.github.com>
2025-02-23 08:48:23 +0530

    hide the footer

--- Home.py
+++ Home.py
@@ -1,3 +1,18 @@
 import streamlit as st
 
+# Use custom CSS to hide the footer icons
+st.markdown(
+    """
+    <style>
+    .css-1d391kg {
+        visibility: hidden;
+    }
+    .css-1gk8k2p {
+        visibility: hidden;
+    }
+    </style>
+    """, 
+    unsafe_allow_html=True
+)
+
 st.title('parthiban Sample')
