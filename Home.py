04b902c53cead5bbddacd54e9443d55f4c58a3a3
parthiban <55436038+parthibanvthink@users.noreply.github.com>
2025-02-23 08:52:36 +0530

    hide header

--- Home.py
+++ Home.py
@@ -1,18 +1,11 @@
 import streamlit as st
 
-# Use custom CSS to hide the footer icons
-st.markdown(
-    """
-    <style>
-    .css-1d391kg {
-        visibility: hidden;
-    }
-    .css-1gk8k2p {
-        visibility: hidden;
-    }
-    </style>
-    """, 
-    unsafe_allow_html=True
-)
+# Design hide top header line
+hide_decoration_bar_style = '''<style>header {visibility: hidden;}</style>'''
+st.markdown(hide_decoration_bar_style, unsafe_allow_html=True)
+# Design hide "made with streamlit" footer menu area
+hide_streamlit_footer = """<style>#MainMenu {visibility: hidden;}
+                        footer {visibility: hidden;}</style>"""
+st.markdown(hide_streamlit_footer, unsafe_allow_html=True)
 
 st.title('parthiban Sample1')
